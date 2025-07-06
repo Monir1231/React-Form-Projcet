461aa47eb104e9379dbe1c3f9ae6741a81fec272
Monir1231 <monirmolla324@gmail.com>
2025-07-06 09:02:59 +0600

    react-form

--- my-project/src/Form.jsx
+++ my-project/src/Form.jsx
@@ -31,7 +31,7 @@ function From(){
     return (
         <>
          <div className=" flex flex-col items-center justify-center h-screen ">
-         <div className=" w-full p-10 max-w-md bg-gray-50  rounded-lg shadow-2xl  hover:scale-105 transition-all duration-300">
+         <div className=" w-full p-10 max-w-md bg-gray-50  rounded-lg shadow-2xl  ">
             <h1 className="text-2xl font-semibold text-gray-800 text-center ">Login from</h1>
             <form >
                 <div className="mt-3">
@@ -54,8 +54,8 @@ function From(){
 
             <div>
                 <h2 className="text-center font-semibold mt-5 ">User from:</h2>
-                <p className="text-gray-300 text-left py-2">Name:{SubmittedData.name}</p>
-                <p className="text-gray-300 text-left">Email:{SubmittedData.email}</p>
+                <p className="text-gray-400 text-left py-2">Name:{SubmittedData.name}</p>
+                <p className="text-gray-400 text-left">Email:{SubmittedData.email}</p>
             </div>
          </div>
 
